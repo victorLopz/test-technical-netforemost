bdf9a3e3dd819183aac2a8eec071307aba7e6c49
victorLopz <castillolopez735@gmail.com>
2024-06-26 12:19:28 -0600

    refactor: improve ArticleListItem widget
    - Refactored the ArticleListItem widget to use a Container for better styling and layout control.
    - Added a boxShadow to the Container to give it a subtle drop shadow effect.
    - Updated the margin and borderRadius properties of the Container for consistent spacing and rounded corners.
    - Moved the ListTile inside the Container to maintain the same structure and functionality.

--- app/lib/widgets/article_list_item.dart
+++ app/lib/widgets/article_list_item.dart
@@ -8,28 +8,43 @@ class ArticleListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: ClipRRect(
-        borderRadius:
-            BorderRadius.circular(8.0), // Borde redondeado de 8 píxeles
-        child: Image.network(
-          article.urlToImage,
-          width: 100,
-          height: 100,
-          fit: BoxFit.cover,
-        ),
-      ),
-      title: Text(
-        article.title,
-        style: TextStyle(fontWeight: FontWeight.bold),
-      ),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(article.author),
-          SizedBox(height: 5),
-          Text(article.description),
+    return Container(
+      margin: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
         ],
+      ),
+      child: ListTile(
+        leading: ClipRRect(
+          borderRadius:
+              BorderRadius.circular(8.0), // Borde redondeado de 8 píxeles
+          child: Image.network(
+            article.urlToImage,
+            width: 100,
+            height: 100,
+            fit: BoxFit.cover,
+          ),
+        ),
+        title: Text(
+          article.title,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(article.author),
+            SizedBox(height: 5),
+            Text(article.description),
+          ],
+        ),
       ),
     );
   }
